7949ad2a6c7634150147c2ce075b06467edff87a
artm44 <art.poleev4@gmail.com>
2024-12-15 19:39:35 +0300

    fix: fix s3 problem load files

--- shareat-backend/src/services/s3_service.py
+++ shareat-backend/src/services/s3_service.py
@@ -1,8 +1,18 @@
 import mimetypes
+from typing import AsyncGenerator
+
 import aioboto3
+from aiohttp import ClientSession
 from botocore.exceptions import NoCredentialsError
 import os
+from botocore.config import Config
 
+CHUNK_SIZE = 1 * 1024 * 1024
+
+config = Config(
+    connect_timeout=10,
+    read_timeout=30
+)
 bucket_name = os.getenv("S3_BUCKET_NAME")
 region = os.getenv("S3_REGION")
 access_key = os.getenv("S3_ACCESS_KEY")
@@ -14,12 +24,14 @@ if not all([bucket_name, region, access_key, secret_key]):
 
 
 class S3Service:
-    def __init__(self, bucket_name: str, region: str, access_key: str, secret_key: str, endpoint_url: str = None):
+    def __init__(self, bucket_name: str, region: str, access_key: str,
+                 secret_key: str, endpoint_url: str = None, config: Config = None):
         self.bucket_name = bucket_name
         self.region = region
         self.access_key = access_key
         self.secret_key = secret_key
         self.endpoint_url = endpoint_url
+        self.config = config
 
     async def upload_file(self, file, file_name: str, folder: str = "") -> str:
         """
@@ -67,32 +79,40 @@ class S3Service:
         except NoCredentialsError:
             raise RuntimeError("AWS credentials not found")
 
-    async def get_file(self, file_key: str) -> tuple:
+    async def get_file(self, file_key: str) -> AsyncGenerator[bytes, None]:
         """
-        Получает файл из S3 и возвращает поток и тип контента.
+        Асинхронно получает файл из S3 и возвращает его частями.
         """
         session = aioboto3.Session()
 
-        try:
-            async with session.client(
+        async with session.client(
                 's3',
                 region_name=self.region,
                 aws_access_key_id=self.access_key,
                 aws_secret_access_key=self.secret_key,
                 endpoint_url=self.endpoint_url,
-            ) as s3_client:
-                response = await s3_client.get_object(Bucket=self.bucket_name, Key=file_key)
-                file_stream = response['Body']
-                content_type, _ = mimetypes.guess_type(file_key)
-                if not content_type:
-                    content_type = 'application/octet-stream'  # Фолбэк на универсальный тип
-                return file_stream, content_type
-        except s3_client.exceptions.NoSuchKey:
-            raise FileNotFoundError("File not found in S3")
-        except NoCredentialsError:
-            raise RuntimeError("AWS credentials not found")
+                config=self.config,
+        ) as s3_client:
+            try:
+                # Узнаем размер файла
+                head = await s3_client.head_object(Bucket=self.bucket_name, Key=file_key)
+                content_length = head["ContentLength"]
+
+                # Читаем файл кусками
+                for offset in range(0, content_length, CHUNK_SIZE):
+                    end = min(offset + CHUNK_SIZE - 1, content_length - 1)
+                    response = await s3_client.get_object(
+                        Bucket=self.bucket_name, Key=file_key, Range=f"bytes={offset}-{end}"
+                    )
+
+                    async with response["Body"] as stream:
+                        yield await stream.read()
+            except s3_client.exceptions.NoSuchKey:
+                raise FileNotFoundError("File not found in S3")
+            except NoCredentialsError:
+                raise RuntimeError("AWS credentials not found")
 
 
 async def get_s3_service() -> S3Service:
     return S3Service(bucket_name=bucket_name, region=region, access_key=access_key,
-                     secret_key=secret_key, endpoint_url=endpoint_url)
+                     secret_key=secret_key, endpoint_url=endpoint_url, config=config)
